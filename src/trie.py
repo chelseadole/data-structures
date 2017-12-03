ab1df91e540661c715c09cd32a26c928c49e9924
CHELSEA DOLE <chelseadole@gmail.com>
2017-12-03 11:26:39 -0800

    changing insert to use a for loop

--- src/trie.py
+++ src/trie.py
@@ -24,19 +24,13 @@ class Trie(object):
         self.size = 0
 
     def insert(self, word):
-        """Insert string into Trie."""
+        """Insert a new word into the tree."""
         current = self.root
         if self.contains(word):
             return
-        elif len(word) == 1:
-            word_node = Node(word)
-            current.children[word] = word_node
-            current = word_node
-        else:
-            while word:
-                current.children.setdefault(word[0], Node(word[0]))
-                current = current.children[word[0]]
-                word = word[1:]
+        for letter in word:
+            current.children.setdefault(letter, Node(letter))
+            current = current.children[letter]
         current.end = True
         self.size += 1
         return
