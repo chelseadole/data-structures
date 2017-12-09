5aa97a213ec816fce41f4bb3d3cd69a9ae9ebd88
CHELSEA DOLE <chelseadole@gmail.com>
2017-12-09 13:38:25 -0800

    wrote bubble sort, no tests.

---
+++ src/test_bubblesort.py
@@ -0,0 +1,10 @@
+"""Implementation of Bubble Sort in Python."""
+
+
+def bubblesort(lst):
+    """Bubble sorting algorithm."""
+    for i in range(len(lst)):
+        for j in range(len(lst) - 1, i, -1):
+            if lst[j] < lst[j - 1]:
+                lst[j], lst[j - 1] = lst[j - 1], lst[j]
+    return lst
