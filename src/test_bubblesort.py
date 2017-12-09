81e094f1e31b826562f76d9c86570a3d1f8e28ae
CHELSEA DOLE <chelseadole@gmail.com>
2017-12-09 13:45:51 -0800

    more error handling test.

--- src/test_bubblesort.py
+++ src/test_bubblesort.py
@@ -16,3 +16,15 @@ def test_bubble_sort_doesnt_work_on_string():
     assert bubblesort(wrong_str) == "BubbleSort takes only lists."
 
 
+def test_bubble_sort_doesnt_work_on_set():
+    """Bubble sort cannot be given set as input."""
+    wrong_set = (1, 2, 3, 4)
+    assert bubblesort(wrong_set) == "BubbleSort takes only lists."
+
+
+def test_bubble_sort_doesnt_work_on_dict():
+    """Bubble sort cannot be given set as input."""
+    wrong_dict = {"1": 1, "2": 2, "3": 3}
+    assert bubblesort(wrong_dict) == "BubbleSort takes only lists."
+
+def 
