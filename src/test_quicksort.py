97ca3802cae8e8262f6d1b13ab63a8c83321e572
CHELSEA DOLE <chelseadole@gmail.com>
2017-12-12 13:58:38 -0800

    adding a few more tests.

--- src/test_quicksort.py
+++ src/test_quicksort.py
@@ -19,3 +19,13 @@ def test_quicksort_on_dict():
     """Run quicksort on set."""
     with pytest.raises(TypeError):
         assert quick_sort({}) == [1, 2]
+
+
+def test_quicksort_on_len_1():
+    """Test quicksort on a list length one."""
+    assert quick_sort([1]) == [1]
+
+
+def test_quicksort_on_len_2():
+    """Test quicksort on a list length one."""
+    assert quick_sort([3, 2]) == [2, 3]
