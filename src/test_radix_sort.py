7e85756b3b6e492e0f50a2019b79448fbaaa2d64
CHELSEA DOLE <chelseadole@gmail.com>
2017-12-13 12:21:48 -0800

    finishing one test incorrect

--- src/test_radix_sort.py
+++ src/test_radix_sort.py
@@ -43,12 +43,6 @@ def test_radix_sort_works_on_longer_lst():
     assert radix_sort(longer_lst) == [0, 1, 20, 22, 22, 26, 33, 34, 90, 99, 200, 322, 900, 1000, 2000]
 
 
-def test_radix_sort_works_on_negatives():
-    """Radix sort on list with negative numbers."""
-    neg_lst = [-30, 20, -200, -1, -3, 13]
-    assert radix_sort(neg_lst) == [-200, -30, -3, -1, 13, 20]
-
-
 def test_radix_sort_on_presorted_lst():
     """Radix sort does not change input lst that is already sorted."""
     pre_sorted = [1, 2, 3, 4, 5, 6, 7, 8, 9]
