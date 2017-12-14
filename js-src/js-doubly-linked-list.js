63d10db370117f65276d79282100cebf95aef7f8
CHELSEA DOLE <chelseadole@gmail.com>
2017-12-14 15:58:12 -0800

    creating files for LL and DLL in js-src file

---
+++ js-src/js-doubly-linked-list.js
@@ -0,0 +1,13 @@
+"use strict";
+
+class Node {
+    constructor(data, nextNode=None, prevNode=None) {
+        this.data = data
+        this.nextNode = nextNode
+        this.prevNode = prevNode
+    }
+}
+
+class DLL {
+    
+}
---
+++ js-src/js-linked-list.js
@@ -0,0 +1,12 @@
+"use strict";
+
+class Node {
+    constructor(data, nextNode=None) {
+        this.data = data
+        this.nextNode = nextNode
+    }
+}
+
+class LL {
+    
+}
