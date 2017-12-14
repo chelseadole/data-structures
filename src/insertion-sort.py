7658d49726deec931f4b848fb966aa6581244aaf
CHELSEA DOLE <chelseadole@gmail.com>
2017-12-14 10:07:22 -0800

    finished insertion sort.

--- src/insertion-sort.py
+++ src/insertion-sort.py
@@ -4,8 +4,13 @@
 def insertion_sort(lst):
     """Implementation of insertion sort in python."""
     if isinstance(lst, list):
-        if 
-        for i in lst[1:]:
-            while lst[i] < lst[i - 1]:
+        for i in range(1, len(lst)):
+            curr = lst[i]
+            if not isinstance(i, int):
+                raise TypeError('Only integers can be in the list.')
+            while i > 0 and lst[i] < lst[i - 1]:
                 lst[i], lst[i - 1] = lst[i - 1], lst[i]
                 i -= 1
+            i = curr
+        return lst
+    raise TypeError('Only lists can be used with Insertion Sort.')
