7145bdc0015604d689a8784cf58adea5bfba2d46
CHELSEA DOLE <chelseadole@gmail.com>
2017-12-14 16:05:35 -0800

    added semi colon bois

--- js-src/js-doubly-linked-list.js
+++ js-src/js-doubly-linked-list.js
@@ -2,12 +2,21 @@
 
 class Node {
     constructor(data, nextNode=None, prevNode=None) {
-        this.data = data
-        this.nextNode = nextNode
-        this.prevNode = prevNode
+        this.data = data;
+        this.nextNode = nextNode;
+        this.prevNode = prevNode;
     }
 }
 
 class DLL {
-    
+    constructor(iter=null) {
+        this.head = null;
+        this.tail = null;
+        if (Array.isArray(iter)) {
+            iterable.forEach(item => this.push(item));
+        }
+        else if (iter != null) {
+            throw 'LinkedList only takes arrays as inputs.';
+        }
+    }
 }
