f406f80c065aa638fe651e1ff81e2c0208b24cd6
CHELSEA DOLE <chelseadole@gmail.com>
2017-12-14 16:03:29 -0800

    adding constructor to LL.

--- js-src/js-linked-list.js
+++ js-src/js-linked-list.js
@@ -1,12 +1,21 @@
 "use strict";
 
 class Node {
-    constructor(data, nextNode=None) {
-        this.data = data
-        this.nextNode = nextNode
+    constructor(data, nextNode=null) {
+        this.data = data;
+        this.nextNode = nextNode;
     }
 }
 
 class LL {
-    
+    constructor(iter=null) {
+        this.head = null;
+        this.counter = 0;
+        if (Array.isArray(iter)) {
+            iterable.forEach(item => this.push(item));
+        }
+        else if (iter != null) {
+            throw 'LinkedList only takes arrays as inputs.';
+        }
+    }
 }
