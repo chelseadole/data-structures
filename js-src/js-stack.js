a98ced0bf58cfcd7661080ac1b2c02582d60d760
CHELSEA DOLE <chelseadole@gmail.com>
2017-12-16 11:50:28 -0800

    adding js-stack contents. Copying linked-list tests to edit.

---
+++ js-src/js-stack.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const LL = require("./js-linked-list")
+// import {LL} from "js-linked-list.js"
+
+class Stack {
+    constructor(iter=null){
+        this.linked = new LL()
+        if(Array.isArray(iter)){
+            iter.forEach(item => this.push(item))
+        }
+    }
+    
+    size(){
+        return this.linked.size()
+    }
+
+    push(val){
+        this.linked.push(val)
+    }
+
+    pop(){
+        return this.linked.pop()
+    }
+
+}
+
+module.exports = Stack
---
+++ js-src/test/js-stack.spec.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var Stack = require('../js-stack');
+var chai = require('chai');
+var expect = chai.expect;
+
+describe('stack.js tests', () => {
+    it('creating a new empty Stack', () => {
+        var testStack = new Stack();
+        expect(testStack.size()).to.equal(0);
+        expect(testStack.head).to.equal(null);
+    });
+
+    it('passing iterable into new Stack and pushing.', () => {
+        var testStack = new Stack([100, 200, 300, 400, 500]);
+        expect(testStack.size()).to.equal(5);
+        expect(testStack.head.data).to.equal(500);
+
+    });
+ 
+    it('testing push method changes head', () => {
+        var testStack = new Stack();
+        testStack.push('yo');
+        expect(testStack.head.data).to.equal('yo');
+    });
+
+    it('test push method adds one to size.', () => {
+        var testStack = new Stack();
+        testStack.push(1);
+        expect(testStack.size()).to.equal(1);
+    });
+
+    it("test pop on non-empty list.", () => {
+        var testStack = new Stack();
+        testStack.push(5);
+        testStack.push(4);
+        testStack.push(3);
+        expect(testStack.head.data).to.equal(3);
+        expect(testStack.pop()).to.equal(3);
+        expect(testStack.head.data).to.equal(4);
+    });
+
+    it("size function works with push and pop", () => {
+        var testStack = new Stack([1, 2, 3, 4, 5]);
+        expect(testStack.size()).to.equal(5);
+        testStack.push(0);
+        expect(testStack.size()).to.equal(6)
+        testStack.pop();
+        expect(testStack.size()).to.equal(5);
+    });
+});
